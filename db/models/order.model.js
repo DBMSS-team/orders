ca9f85fc4abb1b4d5f42b2efb94070190e171a2f
I355102 <sahil.kumar@sap.com>
2020-09-04 20:43:50 +0530

    replaced address_ids with full address

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const addressSchema = require('../../../users/db/models/user.model').addressSchema;
 
 const orderSchema = new mongoose.Schema(
 	{
 		user_id: { type: String, required: true },
 		payment_id: { type: String, required: true },
-		billing_add_id: { type: String, required: true },
-		shipping_add_id: { type: String, required: true },
+		billing_address: { type: addressSchema, required: true },
+		shipping_address: { type: addressSchema, required: true },
 		amount: { type: mongoose.Types.Decimal128, required: true },
 		status_id: { type: String, required: true },
 		products: [
